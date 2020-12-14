f14105929054e755cf7f4699e48d4c839f2727b2
iComputeDaily <henry.haines@thetwelfth.com>
2020-12-13 20:31:20 -0500

    Add gitignore and read config file

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module maze-bot
 
 go 1.15
 
-require github.com/iComputeDaily/maze v0.0.0-20201213165952-f58d4b36da23
+require (
+	github.com/iComputeDaily/maze v0.0.0-20201213165952-f58d4b36da23
+	github.com/pelletier/go-toml v1.8.1
+)
--- main.go
+++ main.go
@@ -3,23 +3,41 @@ package main
 import "fmt"
 import "math/rand"
 import "time"
+import "io/ioutil"
+import toml "github.com/pelletier/go-toml"
 import "github.com/iComputeDaily/maze"
 
 // Represents confic options related to discord
-type discord struct {
+type Discord struct {
 	ProjectName string
 	BotToken string
 	StatusMessage string
+	Prefix string
 }
 
 // Represents the config file
-type config struct {
-	Discord discord
+type Config struct {
+	Discord Discord
 }
 
 func initalize() {
 	// Initalize the random number generator
 	rand.Seed(time.Now().UTC().UnixNano())
+	
+	// Read the config file
+	file, err := ioutil.ReadFile("config.toml")
+	if err != nil {
+		fmt.Println("Failed to read config file: ", err)
+	}
+	
+	// Parse the data
+	config := Config{}
+	err = toml.Unmarshal(file, &config)
+	if err != nil {
+		fmt.Println("Failed to read parse file: ", err)
+	}
+	
+	fmt.Printf("%+v\n", config)
 	
 	return
 }
