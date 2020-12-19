9b0bffa4e5343eead4c9c1fda06e291b32f9467b
iComputeDaily <henry.haines@thetwelfth.com>
2020-12-19 14:50:10 -0500

    Make my code less terible

--- main.go
+++ main.go
@@ -26,14 +26,14 @@ type Config struct {
 	Discord Discord
 }
 
-var log = &logrus.Logger{
-	Out:       os.Stdout,
-	Formatter: new(logrus.TextFormatter),
-	Hooks:     make(logrus.LevelHooks),
-	Level:     logrus.DebugLevel,
+// Holds global information for the server
+type stuff struct {
+	config Config
+	logger *logrus.Logger
+	client *disgord.Client
 }
 
-func initalize() *disgord.Client {
+func (stuff *stuff) initalize() {
 	// Initalize the random number generator
 	rand.Seed(time.Now().UTC().UnixNano())
 	
@@ -44,78 +44,90 @@ func initalize() *disgord.Client {
 	}
 	
 	// Parse the data
-	config := Config{}
-	err = toml.Unmarshal(file, &config)
+	err = toml.Unmarshal(file, &stuff.config)
 	if err != nil {
-		fmt.Println("Failed to read parse file: ", err)
+		fmt.Println("Failed to parse file: ", err)
 	}
 	
-	fmt.Printf("%+v\n", config)
+	// Setup logging
+	stuff.logger = &logrus.Logger{
+		Out:       os.Stdout,
+		Formatter: new(logrus.TextFormatter),
+		Hooks:     make(logrus.LevelHooks),
+		Level:     logrus.DebugLevel,
+	}
 	
 	// Do some bitmath
-	var intent disgord.Intent
-	intent |= disgord.IntentDirectMessages
-	intent |= disgord.IntentDirectMessageReactions
-	intent |= disgord.IntentDirectMessageTyping
-	fmt.Printf("%b\n", intent)
+	// var intent disgord.Intent
+	// intent |= disgord.IntentDirectMessages
+	// intent |= disgord.IntentDirectMessageReactions
+	// intent |= disgord.IntentDirectMessageTyping
+	// fmt.Printf("%b\n", intent)
 	
 	// Make a disgord client based on config
-	client := disgord.New(disgord.Config {
-		BotToken: config.Discord.BotToken,
-//		Intents: intent,
-		Logger: log,
-		ProjectName: config.Discord.ProjectName,
+	stuff.client = disgord.New(disgord.Config {
+		BotToken: stuff.config.Discord.BotToken,
+		// Intents: intent,
+		Logger: stuff.logger,
+		ProjectName: stuff.config.Discord.ProjectName,
 		Presence: &disgord.UpdateStatusPayload{
 			Since: nil,
 			Game: nil,
-			Status: config.Discord.StatusMessage,
+			Status: stuff.config.Discord.StatusMessage,
 			AFK: false,
 		},
 	})
-	
-	return client
 }
 
+// Replys to messages with hey
 func messageCallback(session disgord.Session, msgEvt *disgord.MessageCreate) {
-	fmt.Println(msgEvt.Message.Content)
 	msgEvt.Message.Reply(context.Background(), session, "hey")
 }
 
+// Checks if the event is a message with a common greating
 func isHey(event interface{}) interface{} {
 	var msg *disgord.Message
 	
+	// Turn the event into a message
 	switch t := event.(type) {
 		case *disgord.MessageCreate:
 			msg = t.Message
 		default:
+			// Unless it's not one in witch case cancel
 			return nil
 	}
 	
+	// Check if it's a common greating word
 	if strings.EqualFold(msg.Content, "hey") ||
 		strings.EqualFold(msg.Content, "hello") ||
 		strings.EqualFold(msg.Content, "hi") {
+			// If so continue
 			return event
 		} else {
+			// Otherwise cancel
 			return nil
 		}
 }
 
 func main() {
 	// Set things up and connect to discord
-	client := initalize()
-	defer client.Gateway().StayConnectedUntilInterrupted()
+	var stuff stuff
+	stuff.initalize()
+	defer stuff.client.Gateway().StayConnectedUntilInterrupted()
 	
 	// Print invite link
-	inviteURL, err := client.BotAuthorizeURL()
+	inviteURL, err := stuff.client.BotAuthorizeURL()
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println(inviteURL)
 	
 	// Create filter to avoid loop where bot responds to it's own messages
-	filter, _ := std.NewMsgFilter(context.Background(), client)
-//	filter.SetPrefix(config.Discord.Prefix)
+	filter, _ := std.NewMsgFilter(context.Background(), stuff.client)
+	
+	// Create middleware to log messages
+	logFilter, _ := std.NewLogFilter(stuff.client)
 	
 	// Register basic handler
-	client.Gateway().WithMiddleware(filter.NotByBot, isHey).MessageCreate(messageCallback)
+	stuff.client.Gateway().WithMiddleware(filter.NotByBot, isHey, logFilter.LogMsg).MessageCreate(messageCallback)
 }
