97d08fb577ef452377c50b6e5a7f2f04f7fe7f93
iComputeDaily <henry.haines@thetwelfth.com>
2021-01-17 15:00:41 -0500

    Gen command now accepts types and checks for ilegal sizes.

--- mazes.go
+++ mazes.go
@@ -13,6 +13,10 @@ func (stuff *stuff) getMaze(msg *disgord.Message) (maze.Maze, error) {
 	var width = stuff.config.General.DefaultMazeWidth
 	var height = stuff.config.General.DefaultMazeHeight
 
+	// Make a maze to hold the maze
+	var coolMaze maze.Maze = &maze.GTreeMaze{}
+
+
 	// Get arguments from message
 	args := strings.Split(msg.Content, " ")
 
@@ -21,18 +25,18 @@ func (stuff *stuff) getMaze(msg *disgord.Message) (maze.Maze, error) {
 
 	for i, arg := range args {
 		// Checks weather the arg is a size
-		isSize, err := regexp.MatchString(`^(?i)\d+x\d+$`, arg)
+		isSize, err := regexp.MatchString(`^(?i)-?\d+x-?\d+$`, arg)
 		// Make shure the regrex didn't break
 		if err != nil {
 			return nil, errors.New("Regrex didn't work; This is probobly a bug.")
 		}
 
 		// Checks weather the arg is a type
-		// isType, err := regexp.MatchString(`^(?i)spikey|windy|loopy$`, arg)
+		isType, err := regexp.MatchString(`^(?i)spikey|windy|loopy$`, arg)
 		// Make shure the regrex didn't break
-		// if err != nil {
-		// 	return nil, errors.New("Regrex didn't work; This is probobly a bug.")
-		// }
+		if err != nil {
+			return nil, errors.New("Regrex didn't work; This is probobly a bug.")
+		}
 
 		switch {
 			// If the argument is empty
@@ -41,7 +45,7 @@ func (stuff *stuff) getMaze(msg *disgord.Message) (maze.Maze, error) {
 				break
 
 			// Too many argunments
-			case i >= 2:
+			case i >= 3:
 				return nil, errors.New("Too many arguments!")
 
 			// The argument is a size
@@ -60,7 +64,15 @@ func (stuff *stuff) getMaze(msg *disgord.Message) (maze.Maze, error) {
 				}
 
 			// The argument is a type
-			// case isType:
+			case isType:
+				switch arg {
+				case "windy":
+					coolMaze = &maze.GTreeMaze{}
+				case "spikey":
+					coolMaze = &maze.KruskalMaze{}
+				case "loopy":
+					return nil, errors.New("Type `loopy` not yet implimented.")
+				}
 
 			// The argument is invaled
 			default:
@@ -68,8 +80,13 @@ func (stuff *stuff) getMaze(msg *disgord.Message) (maze.Maze, error) {
 		}
 	}
 
+	// Check to make shure that the size is within limits
+	if !(width >= 2 && width <= 30 &&
+		height >= 2 && height <= 30) {
+		return nil, errors.New("Size must be at least `2x2`, and at most `30x30`(due to discords charachter limit)")
+	}
+
 	// Generate a maze
-	var coolMaze maze.Maze = &maze.GTreeMaze{}
 	coolMaze.Generate(width, height)
 
 	return coolMaze, nil
