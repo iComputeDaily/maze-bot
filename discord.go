760ef2b68976608dab7ea673b8c6f320a7773c73
iComputeDaily <henry.haines@thetwelfth.com>
2021-02-02 11:11:33 -0500

    Move arround some logic and de-spagetify code in preperation for costom prefixes

--- discord.go
+++ discord.go
@@ -12,6 +12,7 @@ import "github.com/andersfylling/disgord"
 import "github.com/andersfylling/disgord/std"
 import "go.uber.org/zap"
 import "encoding/json"
+import "strings"
 
 // Regex to match maze type
 var isTypeRegex *regexp.Regexp
@@ -125,7 +126,7 @@ func (stuff *stuff) initalize() {
 }
 
 // Checks if the event isn't valid
-func isNotValid(event interface{}) interface{} {
+func stripCostomPrefixIfExists(event interface{}) interface{} {
 	var msg *disgord.Message
 	
 	// Turn the event into a message
@@ -137,14 +138,15 @@ func isNotValid(event interface{}) interface{} {
 			return nil
 	}
 
-	// Check if it's a valid comand
-	if !isCmdRegex.MatchString(msg.Content) {
-			// If not continue
-			return event
-		} else {
-			// If so cancel
-			return nil
-		}
+	if strings.HasPrefix(msg.Content, "!maze") {
+		msg.Content = strings.TrimPrefix(msg.Content, "!maze")
+		// Note that disgord.MessageCreate contains only a pointer to the
+		// message so dispite asigning the message to our own varible we still
+		// modified the event.
+		return event
+	} else {
+		return nil
+	}
 }
 
 func main() {
@@ -163,54 +165,18 @@ func main() {
 
 	// Set up channels and workers
 	// BUG(iComputeDaily): No idea how big the buffer should be
-	helpEvtChan := make(chan *disgord.MessageCreate, 100)
-	genEvtChan := make(chan *disgord.MessageCreate, 100)
-	invalidEventChan := make(chan *disgord.MessageCreate, 100)
-
+	msgEventChan := make(chan *disgord.MessageCreate, 100)
 	for workerNum := 0; workerNum < stuff.config.Technical.NumWorkers; workerNum++ {
-		go stuff.worker(helpEvtChan, genEvtChan, invalidEventChan)
+		go stuff.worker(msgEventChan)
 	}
-	
-	// Create filter to avoid loop where bot responds to it's own messages
+
+	// Create a filter so we can check if the message is by the bot
 	filter, _ := std.NewMsgFilter(context.Background(), stuff.client)
-	filter.SetPrefix(stuff.config.General.Prefix + "maze ")
-	
-	// Same as previous but without space
-	filterNoSpace, _ := std.NewMsgFilter(context.Background(), stuff.client)
-	filterNoSpace.SetPrefix(stuff.config.General.Prefix + "maze")
 
-	// Create filter to tell if it's the help command
-	filterHelp, _ := std.NewMsgFilter(context.Background(), stuff.client)
-	filterHelp.SetPrefix("help")
-	
-	// Create filter to tell if its a generation request
-	filterGen, _ := std.NewMsgFilter(context.Background(), stuff.client)
-	filterGen.SetPrefix("gen")
-	
-	// Create middleware to log messages
-	logFilter, _ := std.NewLogFilter(stuff.client)
-	
-	// Register handler for "help" command
+	// Register handler for messages with the prefix
 	stuff.client.Gateway().WithMiddleware(filter.NotByBot, // Make shure message isn't by the bot
 		std.CopyMsgEvt, // Copy the message so that other handlers don't have problems
-		filter.HasPrefix, filter.StripPrefix, // Check if has "!maze "
-		filterHelp.HasPrefix, filterHelp.StripPrefix, // Check if has "help"
-		).MessageCreateChan(helpEvtChan) // Log message and push to channel
-		
-	// Register handler for "generate" command
-	stuff.client.Gateway().WithMiddleware(filter.NotByBot, // Make shure message isn't by the bot
-		std.CopyMsgEvt, // Copy the message so that other handlers don't have problems
-		filter.HasPrefix, filter.StripPrefix, // Check if has "!maze "
-		filterGen.HasPrefix, filterGen.StripPrefix, // Check if has "gen"
-		).MessageCreateChan(genEvtChan) // Push to channel
-
-	// Register handler for only "!maze"
-	stuff.client.Gateway().WithMiddleware(filter.NotByBot, // Make shure message isn't by the bot
-		std.CopyMsgEvt, // Copy the message so that other handlers don't have problems
-		// Check if has "!maze", and log message if so
-		// Note that this will log will log all messeges with "!maze" because we have only checked for that so far
-		filterNoSpace.HasPrefix, logFilter.LogMsg, filterNoSpace.StripPrefix,
-		isNotValid, // Check to see is the message isn't a valid command
-		).MessageCreateChan(invalidEventChan) // Push to channel
+		stripCostomPrefixIfExists, // Checks to see if the message has the prefix, and if so removes
+		).MessageCreateChan(msgEventChan) // Push to channel
 										
 }
--- worker.go
+++ worker.go
@@ -3,64 +3,57 @@ package main
 import "github.com/andersfylling/disgord"
 import "context"
 import "fmt"
+import "strings"
 
 // Listens for messges in the channel and deals with them
-func (stuff *stuff) worker(
-	helpEvtChan <-chan *disgord.MessageCreate,
-	genEvtChan <-chan *disgord.MessageCreate,
-	invalidEventChan <-chan *disgord.MessageCreate) {
+func (stuff *stuff) worker(msgEvtChan <-chan *disgord.MessageCreate) {
 	
-	var helpMsg = stuff.config.General.HelpMessage
+	var helpMsg = strings.ReplaceAll(stuff.config.General.HelpMessage, "<prefix>", "!")
 	
 	for {
 		var msg *disgord.Message
 		
 		// Wait for messages
 		select {
-			case data, ok := <-helpEvtChan:
-				if !ok {
-					stuff.logger.Panic("Help channel is dead!")
-					return
-				}
-				msg = data.Message
-				
-				// Reply with help
-				msg.Reply(context.Background(), stuff.client, helpMsg)
-				
-			case data, ok := <-genEvtChan:
-				if !ok {
-					stuff.logger.Panic("Gen channel is dead!")
-					return
-				}
-				msg = data.Message
-
-				// Get the maze from the message
-				coolMaze, err := stuff.getMaze(msg)
-				if err != nil {
-					msg.Reply(context.Background(), stuff.client, fmt.Sprintln("Error:", err))
-
-				} else {
-					// Reply to the message with the maze
-					msg.Reply(context.Background(), stuff.client, "```maze\n" + coolMaze.Stringify() + "```")
-				}
-
-			case data, ok := <-invalidEventChan:
+			case data, ok := <-msgEvtChan:
 				if !ok {
 					stuff.logger.Panic("Invalid channel is dead!")
 					return
 				}
 				msg = data.Message
 
-				// Find the leftmost command
-				cmd := spaceSepRegex.FindString(msg.Content)
+				msg.Content = strings.TrimSpace(msg.Content)
 
-				// If the user didn't input a command
-				if cmd == "" {
-					msg.Reply(context.Background(), stuff.client,
-					"Error: No command provided. Use `!maze help` for usage help.")
-				} else { // If the did
-					msg.Reply(context.Background(), stuff.client,
-					fmt.Sprintln("Error: Invalid command `", cmd, "`. Use `!maze help` for usage help."))
+				switch {
+					case strings.HasPrefix(msg.Content, "gen"):
+						msg.Content = strings.TrimPrefix(msg.Content, "gen")
+
+						// Get the maze from the message
+						coolMaze, err := stuff.getMaze(msg)
+						if err != nil {
+							msg.Reply(context.Background(), stuff.client, fmt.Sprintln("Error:", err))
+
+						} else {
+							// Reply to the message with the maze
+							msg.Reply(context.Background(), stuff.client, "```maze\n" + coolMaze.Stringify() + "```")
+						}
+
+					case strings.HasPrefix(msg.Content, "help"):
+						// Reply with help
+						msg.Reply(context.Background(), stuff.client, helpMsg)
+
+					default:
+						// Find the leftmost command
+						cmd := spaceSepRegex.FindString(msg.Content)
+
+						// If the user didn't input a command
+						if cmd == "" {
+							msg.Reply(context.Background(), stuff.client,
+							"Error: No command provided. Use `!maze help` for usage help.")
+						} else { // If the did
+							msg.Reply(context.Background(), stuff.client,
+							fmt.Sprintln("Error: Invalid command `", cmd, "`. Use `!maze help` for usage help."))
+						}
 				}
 		}
 	}
